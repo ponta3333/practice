19c832f84eaefd5d890bf9b28d6a15f75bb6b3c5
$ ruby -v <i.i.i.you.vvv.333@gmail.com>
2020-08-09 23:29:09 +0900

    [Finish]8-11

--- javascript/Ajax-Lesson/script.js
+++ javascript/Ajax-Lesson/script.js
@@ -1,9 +1,19 @@
-$('#tab-contents .tab[id != "tab1"]').hide();
-
-$('#tab-menu a').on('click', function(event) {
-	$("#tab-contents .tab").hide();
-	$("#tab-menu .active").removeClass("active");
-	$(this).addClass("active");
-	$($(this).attr("href")).show();
-	event.preventDefault();
-});
+$(function(){
+	$('#btn').on('click', function(){
+		$.ajax({
+			url: "http://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + '143d8362ae8b792cb49c69cf63e551d0',
+			dataType : 'jsonp'
+		}).done(function (data){
+			$('#place').text(data.name);
+			$('#temp_max').text(data.main.temp_max);
+			$('#temp_min').text(data.main.temp_min);
+			$('#humidity').text(data.main.humidity);
+			$('#speed').text(data.wind.speed);
+			$('#weather').text(data.weather[0].main);
+			$('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+			$('img').attr("alt",data.weather[0].main);
+		}).fail(function(data){
+			alert('通信に失敗しました。');
+		});
+	});
+}):
